581960121b463bf905192042c909b353f8942b88
CTuMyJI <ctumyji258456post@gmail.com>
2019-11-23 22:22:36 +0200

    Updates

---
+++ precache-manifest.6afdde69e65cbab0941069808a8b9ac5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc525fe92506b4617006ec1d2197cd3",
+    "url": "/AvocadoProject/index.html"
+  },
+  {
+    "revision": "5a22fb7037858e2c9ea4",
+    "url": "/AvocadoProject/static/css/3.0194b4d1.chunk.css"
+  },
+  {
+    "revision": "df022329ccd63cc1659c",
+    "url": "/AvocadoProject/static/css/4.2c1586bc.chunk.css"
+  },
+  {
+    "revision": "9def9be2aa87d3c8054a",
+    "url": "/AvocadoProject/static/css/main.43cc10b6.chunk.css"
+  },
+  {
+    "revision": "e2edfdd56b2c10036a65",
+    "url": "/AvocadoProject/static/js/2.04f87a6a.chunk.js"
+  },
+  {
+    "revision": "5a22fb7037858e2c9ea4",
+    "url": "/AvocadoProject/static/js/3.0a01cd8a.chunk.js"
+  },
+  {
+    "revision": "df022329ccd63cc1659c",
+    "url": "/AvocadoProject/static/js/4.ba886660.chunk.js"
+  },
+  {
+    "revision": "9def9be2aa87d3c8054a",
+    "url": "/AvocadoProject/static/js/main.870aa65e.chunk.js"
+  },
+  {
+    "revision": "4498d42916200be75b27",
+    "url": "/AvocadoProject/static/js/runtime-main.29c91b68.js"
+  },
+  {
+    "revision": "b4a19bf84b684392230c71580d2284a7",
+    "url": "/AvocadoProject/static/media/preloader.b4a19bf8.gif"
+  },
+  {
+    "revision": "8ed61819c10523903862eddaa3fb237d",
+    "url": "/AvocadoProject/static/media/user.8ed61819.jpg"
+  }
+]);
